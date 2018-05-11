3f10d6538be4641d7a359ab839757c409a620191
Christopher Beltran <christod.beltran@gmail.com>
2018-05-11 04:30:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue (recipes, ingredient, amount) {
  return Object.assign({}, recipes, {[ingredient]: amount});
 }
 
+function destructivelyUpdateObjectWithKeyAndValue (recipes, ingredient, amount) {
+  recipes[ingredient] = amount;
+  return recipes;
+}
